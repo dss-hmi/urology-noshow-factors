d5283c75416df88241d3a097fc1e419552bf3bc3
Andriy V. Koval <koval.andrey@gmail.com>
2020-03-05 21:07:24 -0500

    basic EDA stable

---
+++ analysis/eda/eda.R
@@ -0,0 +1,105 @@
+# knitr::stitch_rmd(script="./___/___.R", output="./___/___/___.md")
+# These first few lines run only when the file is run in RStudio, !!NOT when an Rmd/Rnw file calls it!!
+rm(list=ls(all=TRUE)) #Clear the memory of variables from previous run. This is not called by knitr, because it's above the first chunk.
+cat("\f") # clear console
+
+
+# ---- load-packages -----------------------------------------------------------
+library(ggplot2) #For graphing
+library(magrittr) #Pipes
+library(dplyr) # for shorter function names. but still prefer dplyr:: stems
+library(knitr) # dynamic documents
+library(rmarkdown) # dynamic
+library(kableExtra) # enhanced tables, see http://haozhu233.github.io/kableExtra/awesome_table_in_html.html
+# library(TabularManifest) # exploratory data analysis, see https://github.com/Melinae/TabularManifest
+requireNamespace("knitr", quietly=TRUE)
+requireNamespace("scales", quietly=TRUE) #For formating values in graphs
+requireNamespace("RColorBrewer", quietly=TRUE)
+requireNamespace("dplyr", quietly=TRUE)
+requireNamespace("DT", quietly=TRUE) # for dynamic tables
+
+# ---- load-sources ------------------------------------------------------------
+#Load any source files that contain/define functions, but that don't load any other types of variables
+#   into memory.  Avoid side effects and don't pollute the global environment.
+source("./scripts/common-functions.R") # used in multiple reports
+source("./scripts/graphing/graph-presets.R") # fonts, colors, themes
+
+# ----- declare-globals ----------------
+varnames_varlabels <- c(
+  "patient_id"            = "Patient Id"
+  ,"male"                 = "Gender = Male"
+  ,"reason_for_visit"     = "Reason for Visit / Diagnosis"
+  ,"provider"             = "Service provider"
+  ,"insurance"            = "Insurance"
+  ,"history_noshow"       = "History of No-Show"
+  ,"month_of_appointment" = "Month of Appoinment"
+  ,"pm_appointment"       = "Afternoon Appointment"
+  ,"preferred_language"   = "Preferred Language"
+  ,"returned_to_care"     = "Returned to Care"
+  ,"letter_sent"          = "Was letter sent?"
+)
+
+
+# ---- load-data -------------------------------------------------------------
+ds <- readRDS("./data-unshared/derived/0-greeted.rds")
+# ---- inspect-data -------------------------------------------------------------
+# ds %>% glimpse()
+# ---- tweak-data --------------------------------------------------------------
+
+# ---- basic-table --------------------------------------------------------------
+
+# ---- basic-graph --------------------------------------------------------------
+varnames <- setdiff(names(ds),"patient_id")
+
+item_n <- varnames[1]
+# item_n_label <- varnames_varlabels[item_n]
+
+for(item_i in varnames){
+  cat("\n## ", item_i,"\n" )
+  ds %>% TabularManifest::histogram_discrete(
+    variable_name = item_i,
+    main_title = varnames_varlabels[item_i]
+    ) %>% print()
+  cat("\n")
+}
+
+# Sonata form report structure
+# ---- dev-a-0 ---------------------------------
+# ---- dev-a-1 ---------------------------------
+# ---- dev-a-2 ---------------------------------
+# ---- dev-a-3 ---------------------------------
+# ---- dev-a-4 ---------------------------------
+# ---- dev-a-5 ---------------------------------
+
+# ---- dev-b-0 ---------------------------------
+# ---- dev-b-1 ---------------------------------
+# ---- dev-b-2 ---------------------------------
+# ---- dev-b-3 ---------------------------------
+# ---- dev-b-4 ---------------------------------
+# ---- dev-b-5 ---------------------------------
+
+# ---- recap-0 ---------------------------------
+# ---- recap-1 ---------------------------------
+# ---- recap-2 ---------------------------------
+# ---- recap-3 ---------------------------------
+
+
+# ---- publish ---------------------------------------
+path_report_1 <- "./analysis/eda/eda.Rmd"
+allReports <- c(path_report_1)
+
+pathFilesToBuild <- c(allReports)
+testit::assert("The knitr Rmd files should exist.", base::file.exists(pathFilesToBuild))
+# Build the reports
+for( pathFile in pathFilesToBuild ) {
+
+  rmarkdown::render(input = pathFile,
+                    output_format=c(
+                      "html_document" # set print_format <- "html" in seed-study.R
+                      # "pdf_document"
+                      # ,"md_document"
+                      # "word_document" # set print_format <- "pandoc" in seed-study.R
+                    ),
+                    clean=TRUE)
+}
+
---
+++ analysis/eda/eda.md
@@ -0,0 +1,82 @@
+---
+title: "EDA"
+output:
+  html_document: 
+    highlight: tango
+    keep_md: yes
+    theme: readable
+    toc: yes
+    toc_float: yes
+---
+This report conducts basic exploratory data analysis (EDA) of the collected data. 
+
+<!-- These two chunks should be added in the beginning of every .Rmd that you want to source an .R script -->
+<!--  The 1st mandatory chunck  -->
+<!--  Set the working directory to the repository's base directory -->
+
+
+<!--  The 2nd mandatory chunck  -->
+<!-- Set the report-wide options, and point to the external code file. -->
+
+
+
+<!-- Load 'sourced' R files.  Suppress the output when loading packages. --> 
+
+
+
+<!-- Load the sources.  Suppress the output when loading sources. --> 
+
+
+
+<!-- Load any Global functions and variables declared in the R file.  Suppress the output. --> 
+
+
+<!-- Declare any global functions specific to a Rmd output.  Suppress the output. --> 
+
+
+<!-- Load the datasets.   -->
+
+
+<!-- Inspect the datasets.   -->
+
+
+<!-- Tweak the datasets.   -->
+
+
+# Univariate 
+<!-- Basic graph view.   -->
+
+##  male 
+<img src="figure_rmd/basic-graph-1.png" width="550px" />
+
+##  reason_for_visit 
+<img src="figure_rmd/basic-graph-2.png" width="550px" />
+
+##  provider 
+<img src="figure_rmd/basic-graph-3.png" width="550px" />
+
+##  insurance 
+<img src="figure_rmd/basic-graph-4.png" width="550px" />
+
+##  history_noshow 
+<img src="figure_rmd/basic-graph-5.png" width="550px" />
+
+##  month_of_appointment 
+<img src="figure_rmd/basic-graph-6.png" width="550px" />
+
+##  pm_appointment 
+<img src="figure_rmd/basic-graph-7.png" width="550px" />
+
+##  preferred_language 
+<img src="figure_rmd/basic-graph-8.png" width="550px" />
+
+##  returned_to_care 
+<img src="figure_rmd/basic-graph-9.png" width="550px" />
+
+##  letter_sent 
+<img src="figure_rmd/basic-graph-10.png" width="550px" />
+
+
+
+
+
